f41452f2b49c3992c7ea2c729f5422fddd330961
Taeyeop-Kim <driedflame@gmail.com>
2022-01-05 19:15:34 +0900

    first commit

---
+++ style_transfer/fast_neural_style/neural_style/main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 21:51:26 2022
+
+@author: ty-kim
+"""
---
+++ style_transfer/fast_neural_style/neural_style/style.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import sys
+import time
+import re
+
+import numpy as np
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import torch.onnx
+
+import utils
+from transformer_net import TransformerNet
+from vgg import Vgg16
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def load_model(model_path):
+  with torch.no_grad():
+    style_model = TransformerNet()
+    state_dict = torch.load(model_path)
+    # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+    for k in list(state_dict.keys()):
+      if re.search(r'in\d+\.running_(mean|var)$', k):
+        del state_dict[k]
+    style_model.load_state_dict(state_dict)
+    style_model.to(device)  
+    style_model.eval()
+    return style_model
+
+
+def stylize(style_model, content_image, output_image):
+    
+    content_image = utils.load_image(args.content_image, scale=args.content_scale)
+    content_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+
+    if args.model.endswith(".onnx"):
+        output = stylize_onnx_caffe2(content_image, args)
+    else:
+        with torch.no_grad():
+            style_model = TransformerNet()
+            state_dict = torch.load(args.model)
+            # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+            for k in list(state_dict.keys()):
+                if re.search(r'in\d+\.running_(mean|var)$', k):
+                    del state_dict[k]
+            style_model.load_state_dict(state_dict)
+            style_model.to(device)
+            if args.export_onnx:
+                assert args.export_onnx.endswith(".onnx"), "Export model file should end with .onnx"
+                output = torch.onnx._export(style_model, content_image, args.export_onnx).cpu()
+            else:
+                output = style_model(content_image).cpu()
+    utils.save_image(args.output_image, output[0])
